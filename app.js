3d55764a1d5f2a9faf9ebe96dc493265da11ab38
Abheekrishna <abheekrishna17@gmail.com>
2023-06-20 18:38:24 +0530

    Added number appends in output screen

---
+++ app.js
@@ -0,0 +1,28 @@
+const numberBtn = document.querySelectorAll('[data-numbers]');
+const operationBtn = document.querySelectorAll('[data-operations]');
+const equalsBtn = document.querySelector('[data-equals]');
+const allClearBtn = document.querySelector('[data-allClear]');
+const deleteBtn = document.querySelector('[data-delete]');
+const previousNumberTextDiv = document.querySelector('[data-previousNumberText]');
+const currentNumberTextDiv = document.querySelector('[data-currentNumberText]');
+
+let previousNumber = '';
+let currentNumber = '';
+let lastOperation = '';
+let result = null;
+let haveDot = false;
+
+
+
+numberBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.innerText)
+        if(e.target.innerText === '.' && !haveDot) {
+            haveDot = true;
+        } else if (e.innerText === '.' && haveDot) {
+            return;
+        }
+        currentNumber += e.target.innerText;
+        currentNumberTextDiv.innerText = currentNumber;
+    })
+})
